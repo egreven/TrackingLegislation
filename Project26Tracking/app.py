9adf64067aa4cd0166e7a93f12e8089f430c4281
egreven <65984633+egreven@users.noreply.github.com>
2020-05-28 10:10:25 -0400

    Add files via upload

---
+++ Project26Tracking/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+  
+@app.route("/about")
+def about():
+    return render_template("/about.html")
+    
+@app.route("/contact")
+def contact():
+    return render_template("/contact.html")
+
+@app.route("/mapchar")
+def mapchar():
+    return render_template("/mapchar.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
