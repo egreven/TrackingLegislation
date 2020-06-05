62356fb68ff97b29d9650b713e20471b8024337e
egreven <65984633+egreven@users.noreply.github.com>
2020-06-05 10:51:52 -0400

    Add files via upload
    Files combined from Jack and Ziwen.
    
    Need to change colors and import arcgis links

---
+++ websiteFiles/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+  
+@app.route("/aboutus")
+def aboutus():
+    return render_template("/aboutus.html")
+    
+@app.route("/phydicaldistance")
+def phydicaldistance():
+    return render_template("/phydicaldistance.html")
+
+"""@app.route("/mapchar")
+def mapchar():
+    return render_template("/mapchar.html")
+"""
+
+@app.route("/stateofemergency")
+def stateofemergency():
+    return render_template("/stateofemergency.html")
+
+@app.route("/stayathome")
+def stayathome():
+    return render_template("/stayathome.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    app.run(host = '0.0.0.0', port = 5000)
